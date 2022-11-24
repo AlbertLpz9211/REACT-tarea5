f49ad0c7075b7cd614dff73858037e42020a0b06
AlbertLpz9211 <alberto.lopez_g@outlook.com>
2022-11-24 11:33:42 -0600

    create a protected routes and configuration of loguin screen

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,17 @@ import "./App.css";
 import InputName from "./components/InputName";
 import Pokedex from "./components/Pokedex";
 import PokedexDetail from "./components/PokedexDetail";
+import ProtectedRoutes from "./components/ProtectedRoutes";
 
 function App() {
   return (
     <HashRouter>
       <Routes>
         <Route path="/" element={<InputName />} />
-        <Route path="/pokedex" element={<Pokedex />} />
-        <Route path="/pokedex/:id" elementv={<PokedexDetail />} />
+        <Route element={<ProtectedRoutes />}>
+          <Route path="/pokedex" element={<Pokedex />} />
+          <Route path="/pokedex/:id" elementv={<PokedexDetail />} />
+        </Route>
       </Routes>
     </HashRouter>
   );
--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { useSelector } from "react-redux";
 
 const Pokedex = () => {
-    const userName = useSelector(state => state.userName)
-    return (
-        <div>
-            <h1>POKEDEX</h1>
-            <p>welcome {userName}</p>
-        </div>
-    );
+  const userName = useSelector((state) => state.userName);
+  return (
+    <div>
+      <h1>POKEDEX</h1>
+      <p>welcome {userName}</p>
+    </div>
+  );
 };
 
 export default Pokedex;
